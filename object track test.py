0a61e92e0467d584c4ac76fafb5281ef85488666
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2021-12-16 17:44:43 -0600

    Tweaks

--- object track test.py
+++ object track test.py
@@ -28,11 +28,11 @@ else:
 	OPENCV_OBJECT_TRACKERS = {
 		"csrt": cv2.TrackerCSRT_create,
 		"kcf": cv2.TrackerKCF_create,
-		"boosting": cv2.TrackerBoosting_create,
+		"boosting": cv2.legacy.TrackerBoosting_create,
 		"mil": cv2.TrackerMIL_create,
-		"tld": cv2.TrackerTLD_create,
-		"medianflow": cv2.TrackerMedianFlow_create,
-		"mosse": cv2.TrackerMOSSE_create
+		"tld": cv2.legacy.TrackerTLD_create,
+		"medianflow": cv2.legacy.TrackerMedianFlow_create,
+		"mosse": cv2.legacy.TrackerMOSSE_create
 	}
 	# grab the appropriate object tracker using our dictionary of
 	# OpenCV object tracker objects
